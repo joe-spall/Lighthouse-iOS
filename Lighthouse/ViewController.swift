9949e386a509f61109325a1eb0703c6f404625aa
jspall0327 <jspall98@aol.com>
2017-07-24 16:39:02 -0400

    Clustering
    Added clustering
    Added crime icons for clustering
    Fixed spelling on crime_weight
    Implemented HTTPS

--- Lighthouse/ViewController.swift
+++ Lighthouse/ViewController.swift
@@ -18,19 +18,22 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 
     @IBOutlet weak var crimeCount: CrimeCountView!
     @IBOutlet var mapView: MGLMapView!
+  
     
-    let crimePullURL = "http://www.app-lighthouse.com/app/crimepullcirc.php"
+    let crimePullURL = "https://www.app-lighthouse.com/app/crimepullcirc.php"
     let MONTH_DIFF_WEIGHT:Double = 100
     var storedCrimes:[Crime] = []
     var storedPins:[MGLPointAnnotation] = []
     var currentCrimeEntryString:String = ""
     var currentDangerLevel:String = ""
-    
+    var icon: UIImage!
+    var popup: UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         crimeCount.layer.cornerRadius = 10
+        icon = UIImage(named:"crime_icon")
         mapView.delegate = self
         
         
@@ -68,7 +71,7 @@ class ViewController: UIViewController, MGLMapViewDelegate {
                 createErrorAlert(description: "Location not accessible")
             case .authorizedAlways, .authorizedWhenInUse:
                 showLoadingHUD()
-                drawSearchCircle()
+               // drawSearchCircle()
                 pullCrimes()
                 hideLoadingHUD()
                 
@@ -84,28 +87,121 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     }
     
     func addCrimesToMap(crimeArray:[Crime]){
-        let crimeEntryXML = checkCrimeEntryXML()
+        guard let style = mapView.style else { return }
+        var collection:[MGLPointFeature] = []
         for crime in crimeArray{
-            let point = MGLPointAnnotation()
-            point.coordinate = crime.location
-            var crimeType = crime.typeCrime
-            do{
-                let crimeRead = try crimeEntryXML["danger"]["crime"].withAttribute("type", crimeType)["uname"].element?.text
-                crimeType = (crimeRead?.trimmingCharacters(in: .whitespacesAndNewlines))!
-            }
-            catch{
-                print(error)
-                //error handle
-            }
-            point.title = crimeType
-            point.subtitle = setDate(inputDate: crime.date)
-            storedPins.append(point)
-            mapView.addAnnotation(point)
+            let temp = MGLPointFeature()
+            temp.coordinate = crime.location
+            temp.attributes = ["date":crime.date,"typeCrime":crime.typeCrime]
+            collection.append(temp)
         }
-        crimeCount.setCrimeNumberLabel(number: String(crimeArray.count))
+        let shapeCollectionFeature = MGLShapeCollectionFeature(shapes: collection)
+        let source = MGLShapeSource(identifier: "clusteredCrimes",shape:shapeCollectionFeature, options: [.clustered: true, .clusterRadius: icon.size.width, .maximumZoomLevelForClustering:20])
+        style.addSource(source)
+        
+        // Use a template image so that we can tint it with the `iconColor` runtime styling property.
+        style.setImage(icon.withRenderingMode(.alwaysTemplate), forName: "icon")
+        
+        // Show unclustered features as icons. The `cluster` attribute is built into clustering-enabled source features.
+        let crime = MGLSymbolStyleLayer(identifier: "crime", source: source)
+        crime.iconImageName = MGLStyleValue(rawValue: "icon")
+        crime.iconColor = MGLStyleValue(rawValue: UIColor.darkGray.withAlphaComponent(0.9))
+        crime.predicate = NSPredicate(format: "%K != YES", argumentArray: ["cluster"])
+        style.addLayer(crime)
+        
+        // Color clustered features based on clustered point counts.
+        let stops = [
+            20:  MGLStyleValue(rawValue: UIColor.lightGray),
+            50:  MGLStyleValue(rawValue: UIColor.orange),
+            100: MGLStyleValue(rawValue: UIColor.red),
+            200: MGLStyleValue(rawValue: UIColor.purple)
+        ]
+        
+        // Show clustered features as circles. The `point_count` attribute is built into clustering-enabled source features.
+        let circlesLayer = MGLCircleStyleLayer(identifier: "clusteredCrimes", source: source)
+        circlesLayer.circleRadius = MGLStyleValue(rawValue: NSNumber(value: Double(icon.size.width) / 2))
+        circlesLayer.circleOpacity = MGLStyleValue(rawValue: 0.75)
+        circlesLayer.circleStrokeColor = MGLStyleValue(rawValue: UIColor.white.withAlphaComponent(0.75))
+        circlesLayer.circleStrokeWidth = MGLStyleValue(rawValue: 2)
+        circlesLayer.circleColor = MGLSourceStyleFunction(interpolationMode: .interval,
+                                                          stops: stops,
+                                                          attributeName: "point_count",
+                                                          options: nil)
+        circlesLayer.predicate = NSPredicate(format: "%K == YES", argumentArray: ["cluster"])
+        style.addLayer(circlesLayer)
+        
+        // Label cluster circles with a layer of text indicating feature count. Per text token convention, wrap the attribute in {}.
+        let numbersLayer = MGLSymbolStyleLayer(identifier: "clusteredCrimeNumbers", source: source)
+        numbersLayer.textColor = MGLStyleValue(rawValue: UIColor.white)
+        numbersLayer.textFontSize = MGLStyleValue(rawValue: NSNumber(value: Double(icon.size.width) / 2))
+        numbersLayer.iconAllowsOverlap = MGLStyleValue(rawValue: true)
+        numbersLayer.text = MGLStyleValue(rawValue: "{point_count}")
+        numbersLayer.predicate = NSPredicate(format: "%K == YES", argumentArray: ["cluster"])
+        style.addLayer(numbersLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(_:))))
         
     }
     
+    func handleTap(_ tap: UITapGestureRecognizer) {
+        if tap.state == .ended {
+            let point = tap.location(in: tap.view)
+            let width = icon.size.width
+            let rect = CGRect(x: point.x - width / 2, y: point.y - width / 2, width: width, height: width)
+            
+            let clusters = mapView.visibleFeatures(in: rect, styleLayerIdentifiers: ["clusteredCrimes"])
+            let crimes = mapView.visibleFeatures(in: rect, styleLayerIdentifiers: ["crime"])
+            
+            if clusters.count > 0 {
+                showPopup(false, animated: true)
+                let cluster = clusters.first!
+                mapView.setCenter(cluster.coordinate, zoomLevel: (mapView.zoomLevel + 1), animated: true)
+            } else if crimes.count > 0 {
+                let crime = crimes.first!
+                
+                if popup == nil {
+                    popup = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+                    popup!.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+                    popup!.layer.cornerRadius = 4
+                    popup!.layer.masksToBounds = true
+                    popup!.textAlignment = .center
+                    popup!.lineBreakMode = .byTruncatingTail
+                    popup!.font = UIFont.systemFont(ofSize: 16)
+                    popup!.textColor = UIColor.black
+                    popup!.alpha = 0
+                    view.addSubview(popup!)
+                }
+                popup!.text = (crime.attribute(forKey: "typeCrime")! as! String)
+                let size = (popup!.text! as NSString).size(attributes: [NSFontAttributeName: popup!.font])
+                popup!.bounds = CGRect(x: 0, y: 0, width: size.width, height: size.height).insetBy(dx: -10, dy: -10)
+                let point = mapView.convert(crime.coordinate, toPointTo: mapView)
+                popup!.center = CGPoint(x: point.x, y: point.y - 50)
+                
+                if popup!.alpha < 1 {
+                    showPopup(true, animated: true)
+                }
+                
+            } else {
+                showPopup(false, animated: true)
+            }
+        }
+    }
+    
+    func showPopup(_ shouldShow: Bool, animated: Bool) {
+        let alpha: CGFloat = (shouldShow ? 1 : 0)
+        if animated {
+            UIView.animate(withDuration: 0.25) { [unowned self] in
+                self.popup?.alpha = alpha
+            }
+        } else {
+            popup?.alpha = alpha
+        }
+    }
+    
+    func mapViewRegionIsChanging(_ mapView: MGLMapView) {
+        showPopup(false, animated: false)
+    }
+
     func drawSearchCircle() {
         let coordinate:CLLocationCoordinate2D = (mapView.userLocation?.coordinate)!
         let withMeterRadius = Double(UserDefaults.standard.integer(forKey: "radius"))*0.3048
@@ -287,7 +383,7 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     
     func createErrorAlert(description:String){
         let alert = UIAlertController(title: "Alert", message: description, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
